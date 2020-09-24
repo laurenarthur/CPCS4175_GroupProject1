34835358fc5ed1f22972aa6b1d1e0dc61cb38b3a
Sharon Deloach <deloach_sharon@columbusstate.edu>
2020-09-24 20:55:58 +0000

    schedule updated

--- scheduling.c
+++ scheduling.c
@@ -9,6 +9,9 @@
 #include "policies.h"
 #include "global.h"
 
+/*
+* Prints the policy according to whats selected by the user 
+*/
 void print_policy(){
     if(policy == FCFS){
         printf("Scheduling Policy: FCFS.\n");
@@ -21,23 +24,70 @@ void print_policy(){
     }
 }
 
+/* 
+* First Come First Served policy 
+*/
 int orderFCFS(){
+
+    if(job_queue -> size == 0){
+        printf("There are currently no jobs in the queue to order");
+        return -1;
+    }
+
+    if(job_queue -> size == 1){
+        printf("There is currently only 1 job in the queue, reorder not needed!");
+        return 0;
+    }
+
+    Node *temp_pointer = job_queue -> head; //assigns the current jobQueue as head.
+    
 
     printf("Scheduiling policy is switched to FCFS. All the %d waiting jobs have been rescheduled.\n", job_queue->size);
     policy = FCFS;
     return 0;
 }
 
+/* 
+* Second Job First policy 
+*/
+
 int orderSJF(){
 
-     printf("Scheduiling policy is switched to SJF. All the %d waiting jobs have been rescheduled.\n", job_queue->size);
+    if(job_queue -> size == 0){
+        printf("There are currently no jobs in the queue to order");
+    }
+
+    if(job_queue -> size == 1){
+        printf("There is currently only 1 job in the queue, reorder not needed!");
+        return 0;
+    }
+
+    Node *temp_pointer = job_queue -> head; //assigns the current jobQueue as head.
+
+
+    printf("Scheduiling policy is switched to SJF. All the %d waiting jobs have been rescheduled.\n", job_queue->size);
     policy = FCFS;
     return 0;
 }
 
+/* 
+* Priority policy 
+*/
+
 int orderPriority(){
 
-     printf("Scheduiling policy is switched to Priority. All the %d waiting jobs have been rescheduled.\n", job_queue->size);
+    if(job_queue -> size == 0){
+        printf("There are currently no jobs in the queue to order");
+    }
+
+    if(job_queue -> size == 1){
+        printf("There is currently only 1 job in the queue, reorder not needed!");
+        return 0;
+    }
+
+    Node *temp_pointer = job_queue -> head; //assigns the current jobQueue as head.
+
+    printf("Scheduiling policy is switched to Priority. All the %d waiting jobs have been rescheduled.\n", job_queue->size);
     policy = FCFS;
     return 0;
 }
