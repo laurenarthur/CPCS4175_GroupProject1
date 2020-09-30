5be445a4c98e278897fa609aede79386eeb1bfdf
laurenarthur <arthur_lauren@columbusstate.edu>
2020-09-30 20:04:52 +0000

    updated command line

--- commandline_parser.c
+++ commandline_parser.c
@@ -13,7 +13,6 @@
 #include <string.h>
 
 #include "queue.h"
-#include "policies.h"
 #include "global.h"
 #include "scheduling.h"
 #include "parser.h"
@@ -28,13 +27,13 @@
 void menu_execute(char *line, int isargs); 
 int cmd_run(int nargs, char **args); 
 int cmd_quit(int nargs, char **args); 
-void showmenu(const char *name, const char *x[]);
+void showmenu(const char *x[]);
 int cmd_helpmenu(int n, char **a);
 int cmd_dispatch(char *cmd);
 
-int cmd_fcfs();
-int cmd_sjf();
-int cmd_priority();
+int cmd_fcfs(int nargs);
+int cmd_sjf(int nargs);
+int cmd_priority(int nargs);
 
 int cmd_list(int nargs);
 int cmd_test(int nargs);
@@ -43,33 +42,61 @@ int cmd_test(int nargs);
  * The run command - submit a job.
  */
 int cmd_run(int nargs, char **args) {
-	if (nargs != 4) {
-		printf("Usage: run <job> <time> <priority>\n");
+	if (nargs != 4 && nargs != 3) {
+		printf("Usage: run <job> <time> <priority>\" specify the time and priority.\n");
+        printf("Usage: \"run <job> <time>\" to specify the time. Priority will be set to 1.\n");
 		return EINVAL;
 	}
+
+    Node *newNode = (Node*) malloc(sizeof(Node));
+    printf("Job %s was submitted.\n", args[1]);
+
+    print_policy();
+    newNode->name = (char*) malloc(strlen(args[1]) + 1);
+    strcpy(newNode->name, args[1]);
+    newNode->jobTime = (int) atoi(args[2]);
+
+    if (nargs == 3){
+        newNode->jobPriority = 1;
+    }
+    else{
+        newNode->jobPriority = (int) atoi(args[3]);
+    }
         
-        /* Use execv to run the submitted job in csubatch */
-        printf("use execv to run the job in csubatch.\n");
+        enQueue(newNode);
+        print_num_jobs();
       	return 0; /* if succeed */
 }
 
 /*
  * FCFS command 
  */
-int cmd_fcfs(){
+int cmd_fcfs(int nargs){
+    if(nargs != 1){
+        printf("The \"FCFS\" command does not accept any flags.\n");
+        return EINVAL;
+    }
     return orderFCFS();
 }
 
 /*
  * SJF command
  */
-int cmd_sjf(){
+int cmd_sjf(int nargs){
+    if (nargs != 1){
+        printf("The \"SJF\" command does not accept any flags.\n");
+        return EINVAL;
+    }
     return orderSJF();
 }
  /*
  * Priority command 
  */
- int cmd_priority(){
+ int cmd_priority(int nargs){
+     if(nargs != 1){
+         printf("The \"Prioritty\" command does not accept any flags.\n");
+         return EINVAL;
+     }
     return orderPriority();
 }
 
@@ -77,6 +104,10 @@ int cmd_sjf(){
  * The quit command.
  */
 int cmd_quit(int nargs, char **args) {
+    if(nargs != 1){
+        printf("The \"quit\" command does not accept any flags.\n");
+        return EINVAL;
+    }
 	printf("Please display performance information before exiting csubatch!\n");
         exit(0);
 }
@@ -84,12 +115,12 @@ int cmd_quit(int nargs, char **args) {
 /*
  * Display menu information
  */
-void showmenu(const char *name, const char *x[])
+void showmenu(const char *x[])
 {
 	int ct, half, i;
 
 	printf("\n");
-	printf("%s\n", name);
+	//printf("%s\n", name);
 	
 	for (i=ct=0; x[i]; i++) {
 		ct++;
@@ -98,9 +129,9 @@ void showmenu(const char *name, const char *x[])
 
 	for (i=0; i<half; i++) {
 		printf("    %-36s", x[i]);
-		if (i+half < ct) {
-			printf("%s", x[i+half]);
-		}
+		//if (i+half < ct) {
+		//	printf("%s", x[i+half]);
+		//}
 		printf("\n");
 	}
 
@@ -124,8 +155,21 @@ int cmd_helpmenu(int n, char **a)
 {
 	(void)n;
 	(void)a;
-
-	showmenu("csubatch help menu", helpmenu);
+    if(n==1){
+        showmenu(helpmenu);
+    }
+    else if(n>2){
+        printf("Can only accept one flag at a time\n");
+        printf("Acceptable flags:\"-fcfs\", \"-help\", \"-list\", \"-priority\", \"-quit\", \"-run\", \"-sjf\", \"-test\"\n");
+    }
+    else{
+        if(a[1][0] == "-"){
+            showmenu(helpmenu);
+        }
+        else{
+            printf("\"%s\" is not a valid flag. All flag starts with a hyphen ('-').\n", a[1]);
+        }
+    }
 	return 0;
 }
 
@@ -134,10 +178,11 @@ int cmd_helpmenu(int n, char **a)
  */
 int cmd_list(int nargs) {
 	if(nargs != 1){
-        //print list of jobs in the queue that are running and pending 
+        //print list of jobs in the queue that are running and pending
+        printf("The\"list\" command does not accept any flags.");
         return EINVAL;
     }
-    print_number_jobs();
+    print_num_jobs();
     printQueue();
     print_policy();
     return 0;
@@ -206,13 +251,19 @@ int cmd_dispatch(char *cmd)
 			printf("Command line has too many words\n");
 			return E2BIG;
 		}
-		args[nargs++] = word;
+        if(strcmp(word, " ")!= 0 && strcmp(word, "\n") !=0){
+            args[nargs++] = word;
+        }
 	}
 
-	if (nargs==0) {
+	if (nargs==0) //if nothing was entered 
+    {
 		return 0;
 	}
-
+    for(i=0; i<nargs;i++){
+        args[i] = strtok(args[i], "\n");
+    }
+    //iterates through the commans table to see if the "cmd" wntered is a valid command 
 	for (i=0; cmdtable[i].name; i++) {
 		if (*cmdtable[i].name && !strcmp(args[0], cmdtable[i].name)) {
 			assert(cmdtable[i].func!=NULL);
@@ -222,7 +273,7 @@ int cmd_dispatch(char *cmd)
 			return result;
 		}
 	}
-
+    //Command not found 
 	printf("%s: Command not found\n", args[0]);
 	return EINVAL;
 }
